3dc47d69e668857c7bdcf9b09c0cc5aecde6a2a6
Basak <chgy2131@naver.com>
2023-05-28 02:30:24 +0900

    feat(UiWidgetExample): 화면 스크롤 가능하게 지정 - SingleChildScrollView widget

--- ui_widget_example/lib/main.dart
+++ ui_widget_example/lib/main.dart
@@ -12,104 +12,106 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFF181818),
-        body: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 80,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Text("Hey, Selena",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w800,
-                          )),
-                      Text("Welcome back",
-                          style: TextStyle(
-                              color: Colors.white.withOpacity(0.8),
-                              fontSize: 18)),
-                    ],
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 120,
-              ),
-              Text('Total Balance',
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(0.8),
-                    fontSize: 18,
-                  )),
-              const SizedBox(
-                height: 5,
-              ),
-              const Text('\$5 194 482',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 44,
-                      fontWeight: FontWeight.w600)),
-              const SizedBox(
-                height: 30,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Button(text: "Transfer", bgColor: Colors.amber, textColor: Colors.black),
-                  Button(text: "Request", bgColor: Color(0xFF1F2123), textColor: Colors.white),
-                ],
-              ),
-              const SizedBox(
-                height: 100,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const Text("Wallets", style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 36,
-                      fontWeight: FontWeight.w600,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text("Hey, Selena",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w800,
+                            )),
+                        Text("Welcome back",
+                            style: TextStyle(
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 18)),
+                      ],
                     )
-                  ),
-                  Text("view all", style: TextStyle(
-                    color: Colors.white.withOpacity(0.8),
-                    fontSize: 18,
-                  )),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const CurrencyCard(
-                  name: "Euro",
-                  code: "EUR",
-                  amount: "6 428",
-                  icon: Icons.euro_rounded,
+                  ],
+                ),
+                const SizedBox(
+                  height: 120,
+                ),
+                Text('Total Balance',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 18,
+                    )),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text('\$5 194 482',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 44,
+                        fontWeight: FontWeight.w600)),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Button(text: "Transfer", bgColor: Colors.amber, textColor: Colors.black),
+                    Button(text: "Request", bgColor: Color(0xFF1F2123), textColor: Colors.white),
+                  ],
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text("Wallets", style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600,
+                      )
+                    ),
+                    Text("view all", style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 18,
+                    )),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const CurrencyCard(
+                    name: "Euro",
+                    code: "EUR",
+                    amount: "6 428",
+                    icon: Icons.euro_rounded,
+                    isInverted: true,
+                ),
+                const CurrencyCard(
+                  name: "Bitcoin",
+                  code: "BTC",
+                  amount: "1 234",
+                  icon: Icons.currency_bitcoin_rounded,
+                  isInverted: false,
+                ),
+                const CurrencyCard(
+                  name: "Dollar",
+                  code: "USD",
+                  amount: "9 876",
+                  icon: Icons.attach_money_rounded,
                   isInverted: true,
-              ),
-              const CurrencyCard(
-                name: "Bitcoin",
-                code: "BTC",
-                amount: "1 234",
-                icon: Icons.currency_bitcoin_rounded,
-                isInverted: false,
-              ),
-              const CurrencyCard(
-                name: "Dollar",
-                code: "USD",
-                amount: "9 876",
-                icon: Icons.attach_money_rounded,
-                isInverted: true,
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
