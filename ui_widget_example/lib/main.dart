e924ed21b67b0f1cb913a6ff59b8a9a5aebefc8e
Basak <chgy2131@naver.com>
2023-05-28 01:53:18 +0900

    feat(UiWidgetExample): Card 생성

--- ui_widget_example/lib/main.dart
+++ ui_widget_example/lib/main.dart
@@ -64,7 +64,66 @@ class MyApp extends StatelessWidget {
                   Button(text: "Transfer", bgColor: Colors.amber, textColor: Colors.black),
                   Button(text: "Request", bgColor: Color(0xFF1F2123), textColor: Colors.white),
                 ],
-              )
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text("Wallets", style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontWeight: FontWeight.w600,
+                    )
+                  ),
+                  Text("view all", style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 18,
+                  )),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFF1F2123),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Euro", style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32,
+                            fontWeight: FontWeight.w600,
+                          )),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Text("6 428", style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),),
+                              const SizedBox(width: 5),
+                              Text("EUR", style: TextStyle(
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 20
+                              )),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
