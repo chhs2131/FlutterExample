de14e73d4122baf414f841df86f5007aca3e6c47
Basak <chgy2131@naver.com>
2023-05-28 20:29:15 +0900

    feat(StatefulWidgetExample): Theme 추가 - Theme, TextTheme, BuildContext 사용

--- stateful_widget_example/lib/main.dart
+++ stateful_widget_example/lib/main.dart
@@ -9,24 +9,32 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
+  List<int> numbers = [];
 
   void onClicked() {
     setState(() {  // state에게 새로운 데이터가 있다는 사실을 전달하는 함수 -> 화면 refresh를 위해 build()가 새로 실행됨
-      counter++;  // setState 외부에 있어도 동작에는 지장이 없지만, 가독성을 위해 내부로 넣어준다.
+      numbers.add(numbers.length);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          )
+        )
+      ),
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
             child: Column(
               children: [
-                const Text("click count", style:TextStyle(fontSize:30)),
-                Text('$counter'),
+                MyLargeTitle(),
+                Text('${numbers.length}'),
+                // for(var n in numbers) Text('$n'),
                 IconButton(
                   onPressed: onClicked,
                   icon: const Icon(Icons.add_box_rounded),
@@ -35,6 +43,19 @@ class _AppState extends State<App> {
             ),
         ),
       ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {  // context를 이용하여 부모요소에 접근할 수 있다.
+    return Text("click count", style:TextStyle(
+        fontSize:30, color: Theme.of(context).textTheme.titleLarge!.color),  // null에 대해서 !는 무조건있다. ?는 있으면 행해라!
     );
   }
 }
