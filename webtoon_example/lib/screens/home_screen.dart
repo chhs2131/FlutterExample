6a026cefd567d2b8c0a6eb26cf2329295958d523
Basak <chgy2131@naver.com>
2023-05-30 14:11:43 +0900

    feat(WebtoonExample): stless widget에서 futureBuilder를 통해 동적 페이지 만들기

--- webtoon_example/lib/screens/home_screen.dart
+++ webtoon_example/lib/screens/home_screen.dart
@@ -3,34 +3,13 @@ import 'package:flutter/material.dart';
 import '../models/webtoon_model.dart';
 import '../services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -45,6 +24,15 @@ class _HomeScreenState extends State<HomeScreen> {
         foregroundColor: Colors.green,
       ),
       backgroundColor: Colors.white,
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if(snapshot.hasData) {
+            return Text("There is data!");
+          }
+          return Text("loading");
+        },
+      ),
     );
   }
 }
