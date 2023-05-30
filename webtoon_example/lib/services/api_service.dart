7d84f36b581b967cccd662a9b82201d0657ffb73
Basak <chgy2131@naver.com>
2023-05-30 13:11:38 +0900

    feat(WebtoonExample): model class를 기반으로 api 통신을 하는 기능 추가

--- webtoon_example/lib/services/api_service.dart
+++ webtoon_example/lib/services/api_service.dart
@@ -1,17 +1,26 @@
 
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+
+import '../models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
   // https://pub.dev/packages/http
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {  // async를 통해 반환될 때는 'Future' 를 wrapping 해야한다.
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse("$baseUrl/$today");
     final http.Response response = await http.get(url);
+
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
