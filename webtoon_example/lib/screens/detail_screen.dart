a87ec7e8b141d1ae63320297d9730db17260afa7
Basak <chgy2131@naver.com>
2023-05-31 19:57:48 +0900

    feat(WebtoonExample): 상세페이지 완성 - 웹툰 설명 - 최근 에피소드 확인 및 클릭시 해당 사이트로 이동

--- webtoon_example/lib/screens/detail_screen.dart
+++ webtoon_example/lib/screens/detail_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
-class DetailScreen extends StatelessWidget {
+import '../models/webtoon_detail_model.dart';
+import '../models/webtoon_episode_model.dart';
+import '../services/api_service.dart';
+import '../widgets/episode_widget.dart';
+
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
 
   const DetailScreen({
@@ -11,49 +18,109 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "$title",
-          style: TextStyle(
-            fontSize: 26,
-            fontWeight: FontWeight.w400,
+        appBar: AppBar(
+          title: Text(
+            "${widget.title}", // widget. 은 부모객체(stateful widget)에 접근하라는 뜻
+            style: const TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.w400,
+            ),
           ),
+          elevation: 2,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.green,
         ),
-        elevation: 2,
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.green,
-      ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Hero(
-                tag: id,
-                child: Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 15,
-                          offset: const Offset(10, 10),
-                          color: Colors.black.withOpacity(0.3),
-                        ),
-                      ]),
-                  child: Image.network(thumb),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(50),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Hero(
+                      tag: widget.id,
+                      child: Container(
+                        width: 250,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 15,
+                                offset: const Offset(10, 10),
+                                color: Colors.black.withOpacity(0.3),
+                              ),
+                            ]),
+                        child: Image.network(widget.thumb),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 25,
+                ),
+                FutureBuilder(
+                    future: webtoon,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(snapshot.data!.about,
+                                style: TextStyle(fontSize: 16)),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                                "${snapshot.data!.genre} / ${snapshot.data!.age}",
+                                style: TextStyle(fontSize: 16)),
+                          ],
+                        );
+                      } else {
+                        // return Text("...");
+                        return const CircularProgressIndicator();
+                      }
+                    }),
+                const SizedBox(
+                  height: 50,
+                ),
+                FutureBuilder(
+                  future: episodes,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      // ListViewBuilder를 사용하지 않는 이유는 불러오는 값이 10개로 매우 적기 때문에 이쪽이 더 효율적이다.
+                      return Column(
+                        children: [
+                          for (var episode in snapshot.data!)
+                            Episode(episode: episode, webtoonId: widget.id),
+                        ],
+                      );
+                    }
+                    return Container();
+                  },
+                ),
+              ],
+            ),
           ),
-        ],
-      )
-    );
+        ));
   }
 }
