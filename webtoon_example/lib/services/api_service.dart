6535040362db61487153cd7c4970c3f8ce3af139
Basak <chgy2131@naver.com>
2023-05-31 17:58:35 +0900

    feat(WebtoonExample): 상세페이지 관련 API 통신 추가

--- webtoon_example/lib/services/api_service.dart
+++ webtoon_example/lib/services/api_service.dart
@@ -2,7 +2,9 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:webtoon_example/models/webtoon_episode_model.dart';
 
+import '../models/webtoon_detail_model.dart';
 import '../models/webtoon_model.dart';
 
 class ApiService {
@@ -24,4 +26,30 @@ class ApiService {
     }
     throw Error();
   }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(String id) async {
+    final List<WebtoonEpisodeModel> episodesInstances = [];
+
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
+    }
+    throw Error();
+  }
+
 }
