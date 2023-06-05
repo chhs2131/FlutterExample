66d774168714339bfe4e6103888c06e19b916c4d
Basak <chgy2131@naver.com>
2023-06-06 03:20:14 +0900

    add(FcmExample): 테스트 메시지 수신 기능 확인

--- fcm_example/lib/main.dart
+++ fcm_example/lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:fcm_example/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FCM Test',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'FCM Demo Page'),
     );
   }
 }
@@ -31,13 +35,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String fcmToken = "empty";
+
+  void initFirebase() async {
+    // firebase 초기화
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    // 기기 토큰 확인
+    fcmToken = await FirebaseMessaging.instance.getToken() ?? "empty";
+    print("fcmToken: ${fcmToken}");
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initFirebase();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: const Center(child: Text("hello")));
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text(
+              "[device fcm token]",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Center(child: Text(fcmToken)),
+          ElevatedButton(  // 복사 버튼
+              onPressed: () {
+                Clipboard.setData(ClipboardData(text: fcmToken));
+              },
+              child: const Text("Copy"),
+          )
+        ],
+      ),
+    );
   }
 }
